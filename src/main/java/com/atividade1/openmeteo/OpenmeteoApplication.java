4103502a35c3bbe044f0adb69757ea484e8115b1
JTeixeiraz <138622216+JTeixeiraz@users.noreply.github.com>
2026-08-24 19:18:43 -0300

    chore: initial content

---
+++ src/main/java/com/atividade1/openmeteo/OpenmeteoApplication.java
@@ -0,0 +1,13 @@
+package com.atividade1.openmeteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenmeteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenmeteoApplication.class, args);
+	}
+
+}
